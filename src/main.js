b48bd4e16a2a4fcb14ba0f6542b5a54ce6f79133
apdonaghy <adonaghy@seattleu.edu>
2020-05-31 10:59:33 -0700

    added google analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -9,7 +10,14 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
+Vue.use(VueAnalytics, {
+  id: 'UA-167943063-1',
+  router
+})
+
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
